e699d9e0c561f7b8f025c7a8c21c3aa5d6405e01
mohbadar <mohammadbadarhashimi@gmail.com>
2019-12-07 12:04:28 +0430

    Message Converter

--- src/main/java/af/asr/springaxonkafka/config/kafka/Sender.java
+++
@@ -1,8 +0,0 @@
-package af.asr.springaxonkafka.config.kafka;
-
-import af.asr.springaxonkafka.config.kafka.message.KafkaMessage;
-
-public interface Sender {
-
-    void send(final KafkaMessage message);
-}
---
+++ src/main/java/af/asr/springaxonkafka/sender/MessageConverter.java
@@ -0,0 +1,25 @@
+package af.asr.springaxonkafka.sender;
+
+
+import org.axonframework.kafka.eventhandling.DefaultKafkaMessageConverter;
+import org.axonframework.kafka.eventhandling.KafkaMessageConverter;
+import org.axonframework.serialization.Serializer;
+import org.axonframework.springboot.autoconfig.AxonAutoConfiguration;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@AutoConfigureAfter(AxonAutoConfiguration.class)
+public class MessageConverter {
+
+    @ConditionalOnMissingBean
+    @Bean
+    public KafkaMessageConverter<String, byte[]> kafkaMessageConverter(
+            @Qualifier("eventSerializer") Serializer eventSerializer) {
+        return new DefaultKafkaMessageConverter(eventSerializer);
+    }
+
+}
