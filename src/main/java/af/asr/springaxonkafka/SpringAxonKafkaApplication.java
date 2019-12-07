2dbad9411ea3db43e39af6422b3c631552f3d83c
mohbadar <mohammadbadarhashimi@gmail.com>
2019-12-07 11:00:31 +0430

    setup

---
+++ src/main/java/af/asr/springaxonkafka/SpringAxonKafkaApplication.java
@@ -0,0 +1,13 @@
+package af.asr.springaxonkafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAxonKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAxonKafkaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/af/asr/springaxonkafka/SpringAxonKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package af.asr.springaxonkafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAxonKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
